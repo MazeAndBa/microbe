b31e6413bcda1d3fdd9333e9df4c1f2ab3df01e4
maze0508 <maze0508@gmail.com>
2018-08-07 22:39:13 +0800

    0806

--- Assets/Script/Level/Medium.cs
+++ Assets/Script/Level/Medium.cs
@@ -9,17 +9,23 @@ public class Medium : MonoBehaviour {
     Xmlprocess xmlprocess;
 
     void Start () {
-        btn_practice = GetComponentsInChildren<Button>()[0];
-        btn_compete = GetComponentsInChildren<Button>()[1];
-        btn_back = GetComponentsInChildren<Button>()[2];
+        btn_practice = GetComponentsInChildren<Button>()[1];
+        btn_compete = GetComponentsInChildren<Button>()[2];
+        btn_back = GetComponentsInChildren<Button>()[3];
 
         btn_practice.onClick.AddListener(delegate { goPractice(Home.getLevel()); });
         btn_compete.onClick.AddListener(delegate { goPractice(Home.getLevel()); });
+        btn_back.onClick.AddListener(BackMainmenu);
+
     }
 
     void goPractice(string level) {
         SceneManager.LoadScene(level + "_P");
+    }
 
+    void BackMainmenu()
+    {
+        SceneManager.LoadScene("home");
     }
 
 }
